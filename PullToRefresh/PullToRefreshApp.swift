0c737bd2c0623d4c8e94421fb30d2e6e5f077ef6
makhilgov <makhilgov@ozon.ru>
2022-08-08 09:48:47 +0300

    Initial Commit

---
+++ PullToRefresh/PullToRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullToRefreshApp.swift
+//  PullToRefresh
+//
+//  Created by Akhilgov Magomed Abdulmazhitovich on 08.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PullToRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
